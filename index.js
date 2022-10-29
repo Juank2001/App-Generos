43df822152245d5145e1adc1bd27d11c73fad0da
Juan Vargas <jv288244@gmail.com>
2022-10-28 22:46:15 -0500

    Cargue inical del proyecto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./src/server/server')
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 //starting the server
 app.listen(port, () => {
     console.log(`server on port ${port}`)
